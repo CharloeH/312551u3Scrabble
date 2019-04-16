a0c1592ebda3dc1f1b79b246da5b6b01558ed3f1
Sebastian Horton <36234258+SebastianH11@users.noreply.github.com>
2019-04-16 10:07:48 -0400

    Create Source.cpp

---
+++ SourceFiles/Source.cpp
@@ -0,0 +1,17 @@
+/*
+Sebastian Horton
+4/16/2019
+The program takes 7 letters and forms as many scrabble words as possible with them.
+It also chooses the most valuable option in scrabble points. 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Your Letters are ";
+	
+	return 0;
+}
