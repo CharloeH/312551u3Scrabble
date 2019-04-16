7086ae9528b9ac9a7015feabc20e188b28fa39a6
Sebastian Horton <36234258+SebastianH11@users.noreply.github.com>
2019-04-16 10:07:08 -0400

    Create ScrabbleGame.cpp

---
+++ SourceFiles/ScrabbleGame.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <time.h>     
+
+class ScrabbleGame
+{
+public:
+	ScrabbleGame()
+	{
+		int startingLetter = (char)'A';
+		int total = 0;
+
+		for (int i = startingLetter; i < (startingLetter + 26); i++);
+		{
+			total += 
+		}
+	}
+private:
+
+	int _tilesLeft;
+	int _totalTiles;
+	int r = rand();
+};
+
